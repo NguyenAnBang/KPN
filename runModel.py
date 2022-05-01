66e30e8e971c53582ee6dd13d79ec4462ff036b7
NguyenAnBang <74282590+NguyenAnBang@users.noreply.github.com>
2022-05-01 22:26:34 +0700

    Update runModel.py

--- runModel.py
+++ runModel.py
@@ -31,7 +31,7 @@ else:
 
 Count_Samples = 500000
 Save_Step = 250
-DefaultLambda = 0.7
+DefaultLambda = 0.9
 
 logging.basicConfig(filename=result_log_file, level=logging.INFO)
 logger = logging.getLogger(__name__)
